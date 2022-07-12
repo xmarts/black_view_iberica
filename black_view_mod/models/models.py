404af9ffdcf779d9a431a159dc660e245902a320
juancorreaxsmarts <juan.correa@xmarts.com>
2022-07-12 11:15:56 -0400

    nuevos cambios

---
+++ black_view_mod/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class test_module(models.Model):
+    _inherit = 'mrp.workorder'
+    #_description = 'test_module.test_module'
+
+    paquete_fuente = fields.Many2one('stock.production.lot')
+    #value = fields.Integer()
+    #value2 = fields.Float(compute="_value_pc", store=True)
+    #description = fields.Text()
+
+    #@api.depends('value')
+    #def _value_pc(self):
+    # for record in self:
+    #     record.value2 = float(record.value) / 100
