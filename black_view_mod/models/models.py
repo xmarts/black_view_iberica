bf3b5cda310cfea19df4b981f5f074d78b5babbd
juancorreaxsmarts <juan.correa@xmarts.com>
2022-08-03 01:05:13 -0400

    ultimo_commit

--- black_view_mod/models/models.py
+++ black_view_mod/models/models.py
@@ -1,15 +1,67 @@
 # -*- coding: utf-8 -*-
 
+from asyncio.windows_events import NULL
 from multiprocessing import context
 from odoo import models, fields, api
 from odoo.tools import float_compare, float_round, float_is_zero
+from odoo.addons.mrp_workorder.models.mrp_workorder import MrpProductionWorkcenterLine as original
 
 class MrpProductionWorkcenterLine(models.Model):
     _inherit = 'mrp.workorder'
     #_description = 'test_module.test_module'
 
-    package_id = fields.Many2one(related='move_id.move_line_ids.package_id',readonly=False)
+    package_id = fields.Many2one('stock.quant.package',readonly=False)
     location_id = fields.Many2one(related='move_id.move_line_ids.location_id', readonly=False)
+
+    def _create_extra_move_lines(self):
+        """Create new sml if quantity produced is bigger than the reserved one"""
+        vals_list = []
+        # apply putaway
+        location_dest_id = self.move_id.location_dest_id._get_putaway_strategy(self.product_id)
+        quants = self.env['stock.quant']._gather(self.product_id, self.move_id.location_id, lot_id=self.lot_id, strict=False)
+        # Search for a sub-locations where the product is available.
+        # Loop on the quants to get the locations. If there is not enough
+        # quantity into stock, we take the move location. Anyway, no
+        # reservation is made, so it is still possible to change it afterwards.
+        vals = {
+            'move_id': self.move_id.id,
+            'product_id': self.move_id.product_id.id,
+            'location_dest_id': location_dest_id.id,
+            'product_uom_qty': 0,
+            'product_uom_id': self.move_id.product_uom.id,
+            'lot_id': self.lot_id.id,
+            'recibir':self.package_id.id,
+            'package_id':self.package_id.id,
+            'company_id': self.move_id.company_id.id,
+        }
+        for quant in quants:
+            quantity = quant.quantity - quant.reserved_quantity
+            quantity = self.product_id.uom_id._compute_quantity(quantity, self.product_uom_id, rounding_method='HALF-UP')
+            rounding = quant.product_uom_id.rounding
+            if (float_compare(quant.quantity, 0, precision_rounding=rounding) <= 0 or
+                    float_compare(quantity, 0, precision_rounding=self.product_uom_id.rounding) <= 0):
+                continue
+            vals.update({
+                'location_id': quant.location_id.id,
+                'qty_done': min(quantity, self.qty_done),
+            })
+
+            vals_list.append(vals)
+            self.qty_done -= vals['qty_done']
+            # If all the qty_done is distributed, we can close the loop
+            if float_compare(self.qty_done, 0, precision_rounding=self.product_id.uom_id.rounding) <= 0:
+                break
+
+        if float_compare(self.qty_done, 0, precision_rounding=self.product_id.uom_id.rounding) > 0:
+            vals.update({
+                'location_id': self.move_id.location_id.id,
+                'qty_done': self.qty_done,
+            })
+
+            vals_list.append(vals)
+        return vals_list
+
+    original._create_extra_move_lines = _create_extra_move_lines
 
     @api.onchange('lot_id')
     def _update_component_quantity_lot(self):
@@ -31,25 +83,21 @@ class MrpProductionWorkcenterLine(models.Model):
     def _update_component_quantity_lot_pa(self):
         for r in self:
             if r.package_id:
-                r.move_id.recibir = r.package_id.id
                 if r.qty_done <= -1:
                     r.qty_done = 0
                     if r.qty_done==0:
                         r.component_remaining_qty = 0
 
-
-   
-
-class recibirdatos(models.Model):
-    _inherit = "stock.move"
+class recibirdatolineas(models.Model):
+    _inherit = "stock.move.line"
 
     recibir = fields.Integer()
 
-    @api.onchange('move_line_ids')
-    def _update_component_quantity_lot_pa(self):
-        for r in self:
-            if r.move_line_ids.lot_id:
-                r.move_line_ids.package_id = r.recibir
+#    @api.onchange('lot_id')
+ #   def _update(self):
+  #      for r in self:
+   #         if r.package_id != r.recibir:
+    #            r.package_id = r.recibir
 
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
