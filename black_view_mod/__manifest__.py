e644987301e97ba40a653e01616721d44336c988
marlenenunezxmarts <77165442+marlenenunezxmarts@users.noreply.github.com>
2023-05-23 17:00:49 -0600

    Upd correccion de autor y version

--- black_view_mod/__manifest__.py
+++ black_view_mod/__manifest__.py
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Xmarts",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mrp','mrp_workorder'],
